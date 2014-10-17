89e5bb5d8f5d8475bd64a3890d72cfd2dd4a252d
CaMeLCa5e <jm273606@gmail.com>
2014-10-16 22:25:09 -0400

    Turned application into a function

--- technicalexample2.py
+++ technicalexample2.py
@@ -1,18 +1,17 @@
-
-
-
-
 """
 2. Write a method that accepts a list argument and efficiently finds 
 and returns a distinct list of any duplicate numbers in the list. 
 Include unit tests to verify correct output for a few test lists."""
 
 
-
 #test list
 list_argument = [7, 6, 4, 4, 7, 12, 98] 
-        
-import collections
-print [x for x,y in collections.Counter(list_argument).items() if y>1]        
-        
+
+def findDuplicates(list_argument):
+	import collections
+	print [x for x,y in collections.Counter(list_argument).items() if y>1]
+
+
+
+
 
